775e8b92a8f38566276d6ee5df2c759f2c520d23
lamjq <jake.lam@slu.edu>
2020-09-28 10:16:01 -0500

    setting up project

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("App executed");
+
+  //let x;
+  //x.toString();
+  return (
+    <View style={styles.container}>
+      <Text>Grace you are a cool person</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
