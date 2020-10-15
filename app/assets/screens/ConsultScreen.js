c2e64aa7d17924643fa7f72bdafc1c6968b6cc7f
Leala A. Othman <leala.othman@slu.edu>
2020-10-14 22:22:29 -0500

    Update ConsultScreen.js

--- app/assets/screens/ConsultScreen.js
+++ app/assets/screens/ConsultScreen.js
@@ -1,27 +1,78 @@
-import React from "react";
-import { Button, Alert, Image, StyleSheet, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import hexIMG0 from './photos/hex/1.png';
+import hexIMG1 from './photos/hex/4.png';
+import hexIMG2 from './photos/hex/9.png';
+import hexIMG3 from './photos/hex/34.png';
+import hexIMG4 from './photos/hex/45.png';
+import hexIMG5 from './photos/hex/58.png';
 
-function ConsultScreen(props) {
+import { StyleSheet, Text, View, 
+        Image, ImageBackground, 
+        ScrollView, SafeAreaView } from 'react-native';
+
+const hex1 = [hexIMG0,hexIMG1,hexIMG2,hexIMG3,hexIMG4,hexIMG5];
+
+// const hex = ['./assets/hex/1.png','./assets/hex/4.png', './assets/hex/9.png',
+//  './assets/hex/34.png', './assets/hex/45.png', './assets/hex/58.png'];
+
+
+// Randomization Function... tbh idk why we need it but its here
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+let random = Math.floor(Math.random() * hex1.length);
+// console.log(hex[random], typeof(hex[random]))
+// let image = hex[random];
+// Viewing UI
+export default function ConsultScreen() {
+
+  const handlePress = () => console.log("text pressed");
+  console.log(require("./photos/icon.png"));
   return (
-    <View style={styles.background}>
-      <View style={styles.consultButton}>
-        <Button
-          style={styles.consultButton}
-          title="Enter a question"
-          onPress={() => Alert.prompt("What is your question?")}
-        />
-      </View>
-    </View>
+    <ImageBackground source={require('./photos/MockUpChienv02.jpg')} style={styles.image}>
+      <SafeAreaView style={styles.container}>
+      
+  
+        <Image source={hex1[random]} style={styles.backgroundImage} />
+
+      {/* <Text numberOfLines={1} onPress={handlePress}>Hello World </Text> */}
+      {/* <Image source={require('./assets/icon.png')}/> */}
+      
+
+    </SafeAreaView>
+   
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#e0ffff",
+    alignItems: "center"
+ 
   },
-});
 
-export default ConsultScreen;
+  backgroundImage: {
+    width: 200,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: 'cover', // or 'stretch'
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  }
+  
+});
