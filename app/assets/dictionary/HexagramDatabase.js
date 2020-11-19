a1828ad1b9d9faf54496d5042a3ad38a7dfbc51a
colemanem <elena.coleman@slu.edu>
2020-11-19 17:41:10 -0600

    hex

--- app/assets/dictionary/HexagramDatabase.js
+++ app/assets/dictionary/HexagramDatabase.js
@@ -2,20 +2,44 @@ export const hexData = {
         One: {
             judgement:"Creativity brings sublime success, furthering through perseverance.", 
             image: "The movement of heaven is full of power\nThus the superior person is strong and untiring\n.",
-            lines:"The movement of heaven is full of power\n Thus the superior person is strong and untiring\n Hidden dragon\n Do not act\n Dragon appears in the field\n All day long the superior person is creatively active\n At nightfall their mind is still beset with cares\n Danger\n No blame\n Wavering flight over the depths\n No blame\n Flying dragon in the heavens\n It furthers one to visit the great person\n An arrogant dragon will have cause to repent\n When all the lines are nines, it means: There appears a flight of dragons without heads\n " 
+            lines:"Nine at the beginning means:\n Hidden dragon. Do not act.\n Nine in the second place means:\n Dragon appears in the field.\n It furthers one to visit the great person.\n Nine in the third place means:\n All day long the superior person is creatively active.\n At nightfall their mind is still beset with cares.\n Danger. No blame.\n Nine in the fourth place means:\n Wavering flight over the depths. No blame.\n Nine in the fifth place means:\n Flying dragon in the heavens.\n It furthers one to visit the great person.\n Nine at the top means:\n An arrogant dragon will have cause to repent.\n When all the lines are nines, it means:\n There appears a flight of dragons without heads.\n Good fortune." 
         },
         thirtyFour: {
-            judgement:"The Power of the Great\n",
-            image: "lol",
-            lines: "Perseverance furthers\n Thunder in heaven above: The image of the Power of the Great\n Thus the superior person does not tread upon paths That do not accord with established order\n Power in the toes\n Continuing brings misfortune\n This is certainly true\n The inferior person works through power\n The superior person does not act thus\n To continue is dangerous\n A goat butts against a hedge And gets its horns entangled\n The hedge opens; there is no entanglement\n Power depends upon the axle of a big cart\n Loosens the goat with ease\n No remorse\n A goat butts against a hedge\n It cannot go backward, it cannot go forward\n Nothing serves to further\n If one notes the difficulty, this brings good fortune\n"
+            judgement:"The Power of the Great\n Perseverance furthers.",
+            image: "Thunder in heaven above:\n The image of the Power of the Great.\n Thus the superior person does not tread upon paths\n That do not accord with established order.\n ",
+            lines: "Nine at the beginning means:\n Power in the toes. Continuing brings misfortune.\n This is certainly true.\n Nine in the second place means:\n Perseverance brings good fortune.\n Nine in the third place means:\n The inferior person works through power.\n The superior person does not act thus.\n To continue is dangerous.\n A goat butts against a hedge\n And gets its horns entangled.\n Nine in the fourth place means:\n Perseverance brings good fortune. Remorse disappears.\n The hedge opens; there is no entanglement.\n Power depends upon the axle of a big cart.\n Six in the fifth place means:\n Loosens the goat with ease. No remorse.\n Six at the top means:\n A goat butts against a hedge.\n It cannot go backward, it cannot go forward.\n Nothing serves to further.\n If one notes the difficulty, this brings good fortune."
         },
             
-        five: '',
-        twentySix: '',
-        eleven: '',
-        nine: '',
-        fourteen: '',
-        fortyThree: '',
+        five: {
+            judgement: "Waiting\n If you are sincere, You have light and success\n",
+            image: "Clouds rise up to heaven: The image of waiting\n Thus the superior person eats and drinks,\n Is joyous and of good cheer\n",
+            lines: "Waiting in the meadow\n It furthers one to abide in what endures\n No blame\n Waiting on the sand\n There is some gossip\n The end brings good fortune\n Waiting in the mud\n Brings about the arrival of the enemy\n Waiting in blood\n Get out of the pit\n Waiting at meat and drink\n Perseverance brings good fortune\n One falls into the pit\n Three uninvited guests arrive\n Honor them, and in the end there will be good fortune\n"
+        },
+        twentySix: {
+            judgement: "The Taming Power of the Great. Perseverance furthers.\n Not eating at home brings good fortune. It furthers one to cross the great water.",
+            image: "Heaven within the mountain:\n The image of the Taming Power of the Great.\n Thus the superior person acquaints themselves with many sayings of antiquity\n And many deeds of the past,\n In order to strengthen their character thereby.",
+            lines: "Nine at the beginning means:\n Danger is at hand. It furthers one to desist.\n Nine in the second place means:\n The axletrees are taken from the wagon.\n Nine in the third place means:\n A good horse that follows others.\n Awareness of danger,\n With perseverance, furthers.\n Practice chariot driving and armed defense daily.\n It furthers one to have somewhere to go.\n Six in the fourth place means:\n The headboard of a young bull. Great good fortune.\n Six in the fifth place means:\n The tusk of a gelded boar.\n Good fortune.\n Nine at the top means:\n One attains the way of heaven. Success."
+        },
+        eleven: {
+            judgement: "Peace. The small departs, The great approaches.\n Good fortune. Success.",
+            image: "Heaven and earth unite: the image of Peace. Thus the ruler\n Divides and completes the course of heaven and earth;\n They further and regulate the gifts of heaven and earth,\n And so aid the people.",
+            lines: "Nine at the beginning means:\n When ribbon grass is pulled up, the sod comes with it.\n Each according to their kind.\n Undertakings bring good fortune.\n Nine in the second place means:\n Bearing with the uncultured in gentleness,\n Fording the river with resolution,\n Not neglecting what is distant, Not regarding one's companions:\n Thus one may manage to walk in the middle.\n Nine in the third place means:\n No plain not followed by a slope. No going not followed by a return.\n They who remain persevering in danger\n Is without blame.\n Do not complain about this truth;\n Enjoy the good fortune you still possess.\n Six in the fourth place means:\n They flutter down, not boasting of their wealth,\n Together with their neighbor,\n Guileless and sincere.\n Six in the fifth place means:\n The sovereign\n Gives their child in marriage. This brings blessing\n And supreme good fortune.\n Six at the top means:\n The wall falls back into the moat. Use no army now.\n Make your commands known within your own town.\n Perseverance brings humiliation."
+        },
+        nine: {
+            judgement: "The Taming Power of the Small\n Has success.\n Dense clouds, no rain from our western region.",
+            image: "The wind drives across heaven:\n The image of The Taming Power of the Small.\n Thus the superior person\n Refines the outward aspect of their nature.",
+            lines: "Nine at the beginning means:\n Return to the way.\n How could there be blame in this? Good fortune\n Nine in the second place means:\n They allow themselves to be drawn into returning. Good fortune.\n Nine in the third place means:\n The spokes burst out of the wagon wheels. A married couple rolls their eyes.\n Six in the fourth place means:\n If you are sincere, blood vanishes and fear gives way. No blame.\n Nine in the fifth place means:\n If you are sincere and loyally attached,\n You are rich in neighborliness.\n Nine at the top means:\n The rain comes, there is rest.\n This is due to the lasting effect of character.\n Perseverance brings a person into danger. The moon is nearly full.\n If the superior person persists, misfortune comes."
+        },
+        fourteen: {
+            judgement: "Possession in Great Measure. Supreme success.",
+            image: "Fire in heaven above:\n The image of Possession in Great Measure.\n Thus the superior person curbs evil\n And furthers good,\n And thereby obeys the benevolent will of heaven.",
+            lines: "Nine at the beginning means:\n No relationship with what is harmful;\n There is no blame in this.\n If one remains conscious of difficulty,\n One remains without blame.\n Nine in the second place means:\n A big wagon for loading.\n One may undertake something. No blame.\n Nine in the third place means:\n A ruler offers it to the gods.\n A petty person cannot do this.\n Nine in the fourth place means:\n They makes a difference\n Between themselves and their neighbor. No blame.\n Six in the fifth place means:\n They whose truth is accessible, yet dignified,\n Has good fortune.\n Nine at the top means:\n They are blessed by heaven. Good fortune.\n Nothing that does not further."
+        },
+        fortyThree: {
+            judgement: "Break-through. One must resolutely make the matter known At the court of the king.\n It must be announced truthfully. Danger. It is necessary to notify one's own city.\n It does not further to resort to arms.\n It furthers one to undertake something.",
+            image: "The lake has risen up to heaven: The image of Break-through.\n Thus the superior person\n Dispenses riches downward\n And refrains from resting on their virtue.",
+            lines: "Nine at the beginning means:\n Mighty in the forward-striding toes.\n When one goes and is not equal to the task,\n One makes a mistake.\n Nine in the second place means:\n A cry of alarm.\n Arms at evening and at night.\n Fear nothing.\n Nine in the third place means:\n To be powerful in the cheekbones\n Brings misfortune.\n The superior person is firmly resolved.\n They walk alone and are caught in the rain.\n They are bespattered,\n And people murmur against them. No blame.\n Nine in the fourth place means:\n There is no skin on their thighs,\n And walking comes hard.\n If a person were to let themselves be led like a sheep,\n Remorse would disappear.\n But if these words are heard\n They will not be believed.\n Nine in the fifth place means:\n In dealing with weeds,\n Firm resolution is necessary. Walking in the middle\n Remains free of blame.\n Six at the top means: \n No cry.\n In the end misfortune comes."
+        },
       
         twentyFive: '344333',//chen row
         fiftyOne: '344344',
