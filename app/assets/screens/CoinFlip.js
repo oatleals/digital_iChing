792c61e765b21dbbabfd9884149233c6f5778e61
Leala Othman <leala.othman@slu.edu>
2021-02-02 14:06:18 -0600

    deleted everything

--- app/assets/screens/CoinFlip.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function ConsultScreen() {
-  return <View></View>;
-}
-
-const styles = StyleSheet.create({});
